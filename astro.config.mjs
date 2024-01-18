ffca721de69de6f3185d2c8ff5c664e5c0b788f5
wuyasong <fengchi.wys@raycloud.com>
2024-01-18 17:57:39 +0800

    support more language

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,20 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      "en", // 英语
+      "fr", // 法语
+      // "pt", // 葡萄牙语
+      "es", // 西班牙语
+      "de", // 德语
+      // "it", // 意大利语
+      "pl", // 波兰语
+    ],
+    routing: {
+      prefixDefaultLocale: false
+    },
+  },
 });
