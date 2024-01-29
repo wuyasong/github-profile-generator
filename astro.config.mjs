aec437b3b51a6a3ae9b3ee259152be1cf42c57b1
wuyasong <fengchi.wys@raycloud.com>
2024-01-29 11:42:34 +0800

    i18n add zh and jp

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,8 @@ export default defineConfig({
       "de", // 德语
       // "it", // 意大利语
       "pl", // 波兰语
+      "zh", // 简体中文
+      "jp", // 日语
     ],
     routing: {
       prefixDefaultLocale: false
